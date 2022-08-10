212cc687ba50ffd4fc82a859434743b6f7a93f25
Myles <myles.ieong@gmail.com>
2022-08-10 19:35:59 +0800

    Finish: integrate konfetti view

--- app/src/main/java/com/unicornio/happyinseat/FinishFragment.kt
+++ app/src/main/java/com/unicornio/happyinseat/FinishFragment.kt
@@ -5,11 +5,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_finish.*
+import nl.dionsegijn.konfetti.core.PartyFactory
+import nl.dionsegijn.konfetti.core.Position.Relative
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import nl.dionsegijn.konfetti.core.emitter.EmitterConfig
+import nl.dionsegijn.konfetti.core.models.Shape
+import java.util.concurrent.TimeUnit
 
 class FinishFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(
         R.layout.fragment_finish, container, false
     )
@@ -19,10 +25,35 @@ class FinishFragment : Fragment() {
         Log.d(TAG, "onViewCreated")
 
         setupBehavior(view)
+
+        explode()
+    }
+
+    private fun explode() {
+        val emitterConfig: EmitterConfig = Emitter(100L, TimeUnit.MILLISECONDS).max(100)
+        konfettiView.start(
+            PartyFactory(emitterConfig)
+                .spread(360)
+                .shapes(Shape.Square, Shape.Circle)
+                .colors(listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                .setSpeedBetween(0f, 30f)
+                .position(Relative(0.5, 0.2))
+                .build()
+        )
     }
 
     private fun setupBehavior(root: View) {
-     //TODO
+        button_once_more.setOnClickListener { v ->
+
+        }
+
+        button_finish.setOnClickListener {
+
+        }
+
+        button_start_journey.setOnClickListener {
+
+        }
     }
 
     companion object {
