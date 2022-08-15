27e1852e147cf13d1e208f446aa0d6b7b002a23e
Myles <myles.ieong@gmail.com>
2022-08-15 21:54:27 +0800

    Add class Move, class Exercise and all predefined objects of these classes

---
+++ app/src/main/java/com/unicornio/happyinseat/Move.kt
@@ -0,0 +1,9 @@
+package com.unicornio.happyinseat
+
+data class Move(
+    val name: String,
+    val description: String,
+    val instruction: String,
+    val illustrationId: Int,
+    val reference: String
+)
