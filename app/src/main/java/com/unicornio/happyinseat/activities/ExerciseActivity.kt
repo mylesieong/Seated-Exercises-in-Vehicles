f7538b138d1edfaaa9de2657f06fcaa5ea0bc030
Myles <myles.ieong@gmail.com>
2022-08-19 16:55:10 +0800

    Move: back press will ask user whether to quit

--- app/src/main/java/com/unicornio/happyinseat/activities/ExerciseActivity.kt
+++ app/src/main/java/com/unicornio/happyinseat/activities/ExerciseActivity.kt
@@ -2,12 +2,32 @@ package com.unicornio.happyinseat.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.unicornio.happyinseat.R
+import com.unicornio.happyinseat.askUserWhetherQuit
 
 class ExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_exercise)
+    }
+
+    override fun onBackPressed() {
+        //TODO a better way is to delegate this reaction to fragment but now I can't find a clean way to retrieve the fragment instance from navController
+        val controller = findNavController(R.id.nav_host_fragment)
+        if (controller.currentDestination?.id == R.id.moveFragment) {
+            askUserWhetherQuit(
+                positiveAction = { controller.navigateUp() },
+                negativeAction = {}
+            )
+            return
+        }
+
+        super.onBackPressed()
+    }
+
+    companion object {
+        val TAG = ExerciseActivity::class.java.canonicalName
     }
 }
