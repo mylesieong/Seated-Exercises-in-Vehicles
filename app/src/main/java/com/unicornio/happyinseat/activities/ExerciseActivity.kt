a743eabd992fa6f83d5ef5823afa88c1adec8c6c
Myles <myles.ieong@gmail.com>
2022-11-29 12:18:55 -0800

    Compose: in ExerciseActivity, completely remove fragments and use new compose-navigation

--- app/src/main/java/com/unicornio/happyinseat/activities/ExerciseActivity.kt
+++ app/src/main/java/com/unicornio/happyinseat/activities/ExerciseActivity.kt
@@ -1,30 +1,119 @@
 package com.unicornio.happyinseat.activities
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import com.unicornio.happyinseat.R
-import com.unicornio.happyinseat.askUserWhetherQuit
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.unicornio.happyinseat.AdManager
+import com.unicornio.happyinseat.screens.FinishScreen
+import com.unicornio.happyinseat.screens.MoveScreen
+import com.unicornio.happyinseat.screens.OverviewScreen
 
 class ExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
 
-        setContentView(R.layout.activity_exercise)
+            NavHost(
+                navController = navController,
+                startDestination = "overview"
+
+            ) {
+                composable("overview") {
+                    OverviewScreen(
+                        onExit = {
+                            finishAfterTransition()
+                        },
+                        onNavigateToMove = {
+                            navController.navigate("move/0")
+                        }
+                    )
+                }
+
+                composable(
+                    route = "move/{moveIndex}",
+                    arguments = listOf(navArgument("moveIndex") { type = NavType.IntType })
+                ) {
+                    MoveScreen(
+                        moveIndex = it.arguments?.getInt("moveIndex"),
+                        onExit = {
+                            navController.navigate("overview") {
+                                popUpTo("overview") {
+                                    inclusive = true
+                                }
+                            }
+                        },
+                        onNavigateToMove = { moveIndex ->
+                            navController.navigate("move/$moveIndex") {
+                                popUpTo("overview") {
+                                    inclusive = false
+                                }
+                            }
+                        },
+                        onNavigateToFinish = {
+                            navController.navigate("finish")
+                        }
+                    )
+                }
+
+                composable("finish") {
+                    FinishScreen(
+                        onExit = {
+                            AdManager.loadAndShowInterstitialAdHoweverSilentIfAdsNotReady(
+                                this@ExerciseActivity,
+                                undergoActionIfShowAd = { },
+                                postAction = {
+                                    navController.navigate("overview") {
+                                        popUpTo("overview") {
+                                            inclusive = true
+                                        }
+                                    }
+                                }
+                            )
+                        },
+                        onOnceMore = {
+                            AdManager.loadAndShowInterstitialAdHoweverSilentIfAdsNotReady(
+                                this@ExerciseActivity,
+                                undergoActionIfShowAd = { },
+                                postAction = {
+                                    navController.navigate("overview") {
+                                        popUpTo("overview") {
+                                            inclusive = true
+                                        }
+                                    }
+                                }
+                            )
+                        },
+                        onFinish = {
+                            AdManager.loadAndShowInterstitialAdHoweverSilentIfAdsNotReady(
+                                this@ExerciseActivity,
+                                undergoActionIfShowAd = { },
+                                postAction = {
+                                    finishAfterTransition()
+                                }
+                            )
+                        }
+                    )
+                }
+            }
+        }
     }
 
-    override fun onBackPressed() {
-        //TODO a better way is to delegate this reaction to fragment but now I can't find a clean way to retrieve the fragment instance from navController
-        val controller = findNavController(R.id.nav_host_fragment)
-        if (controller.currentDestination?.id == R.id.moveFragment) {
-            askUserWhetherQuit(
-                positiveAction = { controller.navigateUp() },
-                negativeAction = {}
-            )
-            return
-        }
-
-        super.onBackPressed()
+    private fun debugBackQueue(queue: ArrayDeque<NavBackStackEntry>) {
+        Log.d(
+            TAG, "backQueue=${
+                queue.map {
+                    "id=${it.id}, displayName=${it.destination.displayName}, route=${it.destination.route}, label=${it.destination.label}\n"
+                }
+            }"
+        )
     }
 
     companion object {
