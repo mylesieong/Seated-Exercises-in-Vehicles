e5e62375f262f3c0fcc848a20d00b0181057daf4
Myles <myles.ieong@gmail.com>
2022-11-29 16:59:08 -0800

    Compose:add app bar in main activity and remove all old navigation

--- app/src/main/java/com/unicornio/happyinseat/activities/MainActivity.kt
+++ app/src/main/java/com/unicornio/happyinseat/activities/MainActivity.kt
@@ -1,64 +1,192 @@
 package com.unicornio.happyinseat.activities
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.navigation.findNavController
-import com.google.android.material.navigation.NavigationView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.unicornio.happyinseat.R
-import com.unicornio.happyinseat.databinding.ActivityMainBinding
-import com.unicornio.happyinseat.insertDummyRecordForDebugBuild
+import com.unicornio.happyinseat.fragments.HistoryScreen
+import com.unicornio.happyinseat.fragments.HomeScreen
+import com.unicornio.happyinseat.fragments.SettingScreen
+import com.unicornio.happyinseat.ui.theme.ApplicationTheme
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.common_open_on_phone, R.string.common_open_on_phone)
-
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
-        navigationView.setNavigationItemSelectedListener(this)
-        navigationView.setCheckedItem(R.id.menu_nav_home)
-
-        findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener { controller, destination, bundle ->
-            navigationView.setCheckedItem(
-                when (destination.label) {
-                    "fragment_home" -> R.id.menu_nav_home
-                    "fragment_history" -> R.id.menu_nav_history
-                    "fragment_setting" -> R.id.menu_nav_setting
-                    else -> R.id.menu_nav_home
+        setContent {
+            ApplicationTheme {
+                Surface {
+                    MainScreen()
                 }
-            )
+            }
         }
+    }
+}
 
-        insertDummyRecordForDebugBuild(this)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+
+    val scope = rememberCoroutineScope()
+    val openDrawer = {
+        scope.launch {
+            drawerState.open()
+        }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_nav_home -> {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment)
-            }
-            R.id.menu_nav_history -> {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.action_historyFragment)
-            }
-            R.id.menu_nav_setting -> {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.action_settingFragment)
+    val closeDrawer = {
+        scope.launch {
+            drawerState.open()
+        }
+    }
+
+    ModalDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = true,
+        drawerBackgroundColor = Color.Black,
+        drawerContent = {
+            Column(modifier = Modifier.padding(horizontal = 24.dp)) {
+                Spacer(modifier = Modifier.height(100.dp))
+
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(Color.Yellow)
+                        .padding(16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_logo_foreground), contentDescription = null,
+                        modifier = Modifier
+                            .height(64.dp)
+                            .width(64.dp)
+                    )
+                }
+
+                Spacer(Modifier.height(24.dp))
+
+                MenuItem("home", R.drawable.ic_baseline_home_24) {
+                    navController.navigate("home") {
+                        popUpTo("home") {
+                            inclusive = true
+                        }
+                    }
+
+                    closeDrawer.invoke()
+                }
+
+                Spacer(Modifier.height(24.dp))
+
+                MenuItem("history", R.drawable.ic_baseline_history_24) {
+                    navController.navigate("history") {
+                        popUpTo("history") {
+                            inclusive = true
+                        }
+                    }
+
+                    closeDrawer.invoke()
+                }
+
+                Spacer(Modifier.height(24.dp))
+
+                MenuItem("setting", R.drawable.ic_baseline_settings_24) {
+                    navController.navigate("setting") {
+                        popUpTo("setting") {
+                            inclusive = true
+                        }
+                    }
+
+                    closeDrawer.invoke()
+                }
+
             }
         }
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
-        return true
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "home"
+        ) {
+            composable("home") {
+                Column(Modifier.fillMaxSize()) {
+                    MyTopAppBar("Home", openDrawer)
+                    HomeScreen()
+                }
+            }
+
+            composable("history") {
+                Column(Modifier.fillMaxSize()) {
+                    MyTopAppBar("History", openDrawer)
+                    HistoryScreen()
+                }
+            }
+
+            composable("setting") {
+                Column(Modifier.fillMaxSize()) {
+                    MyTopAppBar("Setting", openDrawer)
+                    SettingScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MenuItem(title: String, iconResId: Int, onClick: () -> Unit) {
+    Row {
+        Image(painter = painterResource(id = iconResId), contentDescription = null)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = title,
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier.clickable { onClick.invoke() }
+        )
+    }
+}
+
+@Composable
+private fun MyTopAppBar(title: String, openDrawer: () -> Job) {
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                openDrawer.invoke()
+            }) {
+                Icon(Icons.Filled.Menu, contentDescription = null)
+            }
+        },
+    )
+}
+
+@Preview(name = "Day mode")
+@Preview(name = "Night mode", uiMode = UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    ApplicationTheme {
+        Surface {
+            MainScreen()
+        }
     }
 }
---
+++ app/src/main/java/com/unicornio/happyinseat/screens/SettingScreen.kt
@@ -0,0 +1,82 @@
+package com.unicornio.happyinseat.fragments
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.TopEnd
+import androidx.compose.ui.Alignment.Companion.TopStart
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.unicornio.happyinseat.R
+import com.unicornio.happyinseat.deleteRecords
+import com.unicornio.happyinseat.helpers.rateApp
+import com.unicornio.happyinseat.ui.theme.ApplicationTheme
+import com.unicornio.toolish.utils.Utils.shotToast
+
+@Composable
+fun SettingScreen() {
+    val context = LocalView.current.context
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+    ) {
+        Text(text = "General", color = contentColorFor(colors.background))
+
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "Remove all records", color = contentColorFor(colors.background), modifier = Modifier.align(TopStart)
+            )
+            Button(modifier = Modifier.align(TopEnd), colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow), onClick = {
+                MaterialAlertDialogBuilder(context).setTitle("Are you sure?").setPositiveButton("Yes") { _, _ ->
+                    deleteRecords(context)
+                    context.shotToast("All records are removed")
+                }.setNegativeButton("No") { dialog, _ -> dialog.cancel() }.show()
+            }) {
+                Text(text = "Remove", color = contentColorFor(colors.primary))
+            }
+        }
+
+        Text(text = "About", color = contentColorFor(colors.background))
+
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "Rate and feedback", color = contentColorFor(colors.background), modifier = Modifier.align(TopStart)
+            )
+            Image(painter = painterResource(id = R.drawable.ic_baseline_insert_comment_24), contentDescription = null, modifier = Modifier
+                .align(TopEnd)
+                .clickable {
+                    rateApp(context)
+                })
+        }
+    }
+
+}
+
+@Composable
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+fun SettingScreenPreview() {
+    ApplicationTheme {
+        Surface {
+            SettingScreen()
+        }
+    }
+}
