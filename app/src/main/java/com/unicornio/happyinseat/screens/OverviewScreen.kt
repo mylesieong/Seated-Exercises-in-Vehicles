725b7a3d6c2e51a354a9985cd452c86083e1ab59
Myles <myles.ieong@gmail.com>
2022-11-30 15:54:03 -0800

    Polish colors

--- app/src/main/java/com/unicornio/happyinseat/screens/OverviewScreen.kt
+++ app/src/main/java/com/unicornio/happyinseat/screens/OverviewScreen.kt
@@ -11,9 +11,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter.Companion.tint
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.unicornio.happyinseat.AnalyticsManager
@@ -46,9 +46,9 @@ fun OverviewScreen(onExit: () -> Unit, onNavigateToMove: () -> Unit) {
 
         Button(modifier = Modifier
             .align(Alignment.BottomCenter)
-            .padding(32.dp),
+            .padding(48.dp),
             shape = RoundedCornerShape(50),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Yellow),
+            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primaryVariant),
             onClick = { startExercise() }) {
             Text(text = "Start", modifier = Modifier.padding(horizontal = 48.dp))
         }
@@ -61,7 +61,7 @@ private fun Header(onExit: () -> Unit) {
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .background(Color.LightGray)
+            .background(MaterialTheme.colors.primary)
     ) {
 
         Image(
@@ -70,20 +70,24 @@ private fun Header(onExit: () -> Unit) {
                 .width(24.dp)
                 .height(24.dp)
                 .clickable { onExit.invoke() },
-            colorFilter = tint(Color.Black),
+            colorFilter = tint(MaterialTheme.colors.onPrimary),
             painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
             contentDescription = null
         )
 
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
-                text = "In Seat/ Stretching", modifier = Modifier.padding(top = 24.dp), style = MaterialTheme.typography.h6
+                text = "In Seat/ Stretching",
+                modifier = Modifier.padding(top = 24.dp),
+                style = MaterialTheme.typography.h6,
+                color = contentColorFor(MaterialTheme.colors.primary)
             )
 
             Text(
                 text = "When travel a long trip in a confined seat, stretching will help reduce neck pain and back pain, improve the blood circulation as long as lower the risk of DVT(Deep Vein Thrombosis).",
                 modifier = Modifier.padding(24.dp),
-                style = MaterialTheme.typography.caption
+                style = MaterialTheme.typography.caption,
+                color = contentColorFor(MaterialTheme.colors.primary)
             )
 
             Row(
@@ -108,9 +112,17 @@ private fun Header(onExit: () -> Unit) {
 @Composable
 fun StatisticCard(name: String, value: String) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = name, style = MaterialTheme.typography.subtitle2)
+        Text(
+            text = name,
+            style = MaterialTheme.typography.subtitle2,
+            color = contentColorFor(MaterialTheme.colors.primary)
+        )
         Spacer(modifier = Modifier.height(4.dp))
-        Text(text = value, style = MaterialTheme.typography.subtitle1)
+        Text(
+            text = value,
+            style = MaterialTheme.typography.subtitle1,
+            color = contentColorFor(MaterialTheme.colors.primary)
+        )
     }
 }
 
@@ -124,7 +136,8 @@ fun MoveListItem(seq: Int, name: String, description: String, resource: Int) {
         Row(
             modifier = Modifier.weight(1f)
         ) {
-            Text(text = seq.toString(), style = MaterialTheme.typography.caption)
+            Text(text = seq.toString(), style = MaterialTheme.typography.caption, fontStyle = FontStyle.Italic)
+
             Spacer(modifier = Modifier.width(8.dp))
 
             Column {
@@ -146,6 +159,6 @@ fun MoveListItem(seq: Int, name: String, description: String, resource: Int) {
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
 fun OverviewScreenPreview() {
     ApplicationTheme {
-            OverviewScreen({}, {})
+        OverviewScreen({}, {})
     }
 }
