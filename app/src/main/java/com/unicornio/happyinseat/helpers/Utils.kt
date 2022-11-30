6c5dcb2a1bf495ae7de41edbe21533ab8c82b860
Myles <myles.ieong@gmail.com>
2022-11-29 22:53:23 -0800

    Compose: clean up unused xml

--- app/src/main/java/com/unicornio/happyinseat/helpers/Utils.kt
+++ app/src/main/java/com/unicornio/happyinseat/helpers/Utils.kt
@@ -1,23 +1,15 @@
 package com.unicornio.happyinseat.helpers
 
-import android.os.Bundle
 import android.util.Log
-import androidx.navigation.NavController
+import androidx.navigation.NavBackStackEntry
 
 const val TAG = "Utils.kt"
 
-fun NavController.navigateSafely(action: Int) {
-    try {
-        navigate(action)
-    } catch (e: IllegalArgumentException) {
-        Log.w(TAG, "navigateSafely: navigation exception when checking current destination and action")
-    }
-}
-
-fun NavController.navigateSafely(action: Int, bundle: Bundle) {
-    try {
-        navigate(action, bundle)
-    } catch (e: IllegalArgumentException) {
-        Log.w(TAG, "navigateSafely: navigation exception when checking current destination and action")
-    }
+fun debugBackQueue(queue: ArrayDeque<NavBackStackEntry>) {
+    Log.d(TAG, "debugBackQueue: ${
+        queue.map {
+            "id=${it.id}, displayName=${it.destination.displayName}, route=${it.destination.route}, label=${it.destination.label}\n"
+        }
+    }"
+    )
 }
