9dc2818aa622a155e84712160ff6f9705d0614b2
Myles <myles.ieong@gmail.com>
2022-11-30 18:41:09 -0800

    Polish Menu item and understand what padding and background is going on

--- app/src/main/java/com/unicornio/happyinseat/activities/MainActivity.kt
+++ app/src/main/java/com/unicornio/happyinseat/activities/MainActivity.kt
@@ -16,9 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -27,6 +29,7 @@ import com.unicornio.happyinseat.screens.HistoryScreen
 import com.unicornio.happyinseat.screens.HomeScreen
 import com.unicornio.happyinseat.screens.SettingScreen
 import com.unicornio.happyinseat.ui.theme.ApplicationTheme
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
@@ -66,60 +69,7 @@ fun MainScreen() {
         gesturesEnabled = true,
         drawerBackgroundColor = MaterialTheme.colors.surface,
         drawerContent = {
-            Column(modifier = Modifier.padding(horizontal = 24.dp)) {
-                Spacer(modifier = Modifier.height(100.dp))
-
-                Box(
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colors.primary)
-                        .padding(16.dp)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_logo_foreground), contentDescription = null,
-                        modifier = Modifier
-                            .height(64.dp)
-                            .width(64.dp)
-                    )
-                }
-
-                Spacer(Modifier.height(24.dp))
-
-                MenuItem("home", R.drawable.ic_baseline_home_24) {
-                    navController.navigate("home") {
-                        popUpTo("home") {
-                            inclusive = true
-                        }
-                    }
-
-                    closeDrawer.invoke()
-                }
-
-                Spacer(Modifier.height(24.dp))
-
-                MenuItem("history", R.drawable.ic_baseline_history_24) {
-                    navController.navigate("history") {
-                        popUpTo("history") {
-                            inclusive = true
-                        }
-                    }
-
-                    closeDrawer.invoke()
-                }
-
-                Spacer(Modifier.height(24.dp))
-
-                MenuItem("setting", R.drawable.ic_baseline_settings_24) {
-                    navController.navigate("setting") {
-                        popUpTo("setting") {
-                            inclusive = true
-                        }
-                    }
-
-                    closeDrawer.invoke()
-                }
-
-            }
+            MyDrawer(navController, closeDrawer)
         }
     ) {
         NavHost(
@@ -151,15 +101,96 @@ fun MainScreen() {
 }
 
 @Composable
+fun MyDrawer(navController: NavHostController, closeDrawer: () -> Job) {
+    Column {
+        Spacer(modifier = Modifier.height(100.dp))
+
+        Box(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colors.primary)
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo_foreground), contentDescription = null,
+                modifier = Modifier
+                    .height(64.dp)
+                    .width(64.dp)
+            )
+        }
+
+        Spacer(Modifier.height(24.dp))
+
+        MenuItem("home", R.drawable.ic_baseline_home_24) {
+            navController.navigate("home") {
+                popUpTo("home") {
+                    inclusive = true
+                }
+            }
+
+            closeDrawer.invoke()
+        }
+
+        MenuItem("history", R.drawable.ic_baseline_history_24) {
+            navController.navigate("history") {
+                popUpTo("history") {
+                    inclusive = true
+                }
+            }
+
+            closeDrawer.invoke()
+        }
+
+        MenuItem("setting", R.drawable.ic_baseline_settings_24) {
+            navController.navigate("setting") {
+                popUpTo("setting") {
+                    inclusive = true
+                }
+            }
+
+            closeDrawer.invoke()
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun MyDrawerPreview() {
+    val context = LocalView.current.context
+    ApplicationTheme {
+        MyDrawer(navController = NavHostController(context)) {
+            GlobalScope.launch {}
+        }
+    }
+}
+
+@Composable
 fun MenuItem(title: String, iconResId: Int, onClick: () -> Unit) {
-    Row {
-        Image(painter = painterResource(id = iconResId), contentDescription = null)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick.invoke() }
+            .padding(12.dp)
+    ) {
         Spacer(modifier = Modifier.width(8.dp))
+        Image(painter = painterResource(id = iconResId), contentDescription = null)
+        Spacer(modifier = Modifier.width(12.dp))
         Text(
             text = title,
             style = MaterialTheme.typography.subtitle1,
-            modifier = Modifier.clickable { onClick.invoke() }
         )
+    }
+}
+
+@Composable
+@Preview
+fun MenuItemPreview() {
+    ApplicationTheme {
+        Surface {
+            MenuItem(title = "title", iconResId = R.drawable.ic_baseline_home_24) {}
+        }
     }
 }
 
