72d0b230d9867c24ac15ba52eec8b501647e5219
Huishi <anhuishi95@gmail.com>
2023-03-01 15:29:48 -0800

    Add elevation for SideMenu

--- react-app/src/SideMenu.js
+++ react-app/src/SideMenu.js
@@ -28,5 +28,14 @@ const styles = StyleSheet.create({
         height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-    },
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity: 0.39,
+        shadowRadius: 8.30,
+
+        elevation: 13,
+    }
 })
