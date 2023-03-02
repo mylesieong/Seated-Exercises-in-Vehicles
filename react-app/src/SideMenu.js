38078308a978546fb871c193ee8121c3e359cb24
j3rry <j3rryzh@gmail.com>
2023-03-01 17:06:35 -0800

    modified code format

--- react-app/src/SideMenu.js
+++ react-app/src/SideMenu.js
@@ -1,40 +1,42 @@
 import { useNavigation } from '@react-navigation/native'
 import { StyleSheet, View, Button } from 'react-native'
+import React from 'react'
 
 export default function SideMenu({ setShowMenu }) {
-    const navigation = useNavigation();
-    const handlePress = (screen) => {
-        navigation.navigate(screen);
-        setShowMenu(false);
-    }
+  const navigation = useNavigation()
+  const handlePress = (screen) => {
+    navigation.navigate(screen)
+    setShowMenu(false)
+  }
 
-    return (
-        <View style={styles.container}>
-            <Button title="Home" onPress={() => handlePress('Home')} />
-            <Button title="Setting" onPress={() => handlePress('Setting')} />
-            <Button title="History" onPress={() => handlePress('History')} />
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Button title='Home' onPress={() => handlePress('Home')} />
+      <Button title='Setting' onPress={() => handlePress('Setting')} />
+      <Button title='History' onPress={() => handlePress('History')} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        left: 0,
-        zIndex: 10,
-        backgroundColor: '#D0D7DD',
-        width: '70%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 12,
-        },
-        shadowOpacity: 0.58,
-        shadowRadius: 16.00,
+  container: {
+    position: 'absolute',
+    top: 10,
+    left: 0,
+    zIndex: 10,
+    backgroundColor: '#D0D7DD',
+    width: '60%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6
+    },
+    shadowOpacity: 0.39,
+    shadowRadius: 8.3,
 
-        elevation: 24,
-    }
+    elevation: 13
+  }
 })
