c311c742d9a5f8ea556df78bda39d37f6da80cb0
Huishi <anhuishi95@gmail.com>
2023-03-02 15:51:23 -0800

    Add icons to SideMenu

--- react-app/src/SideMenu.js
+++ react-app/src/SideMenu.js
@@ -1,5 +1,6 @@
 import { useNavigation } from '@react-navigation/native'
-import { StyleSheet, View, Button, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, View, Pressable, Text, TouchableWithoutFeedback } from 'react-native'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 export default function SideMenu({ setShowMenu }) {
   const navigation = useNavigation()
@@ -14,9 +15,18 @@ export default function SideMenu({ setShowMenu }) {
   return (
     <TouchableWithoutFeedback onPress={handleScreenPress}>
       <View style={styles.container} onPress={handleScreenPress}>
-        <Button title='Home' onPress={() => handlePress('Home')} />
-        <Button title='Setting' onPress={() => handlePress('Setting')} />
-        <Button title='History' onPress={() => handlePress('History')} />
+        <Pressable style={styles.page} onPress={() => handlePress('Home')}>
+          <FontAwesome name='home' style={styles.icon} />
+          <Text style={styles.text}>Home</Text>
+        </Pressable>
+        <Pressable style={styles.page} onPress={() => handlePress('Setting')}>
+          <FontAwesome name='cog' style={styles.icon} />
+          <Text style={styles.text}>Setting</Text>
+        </Pressable>
+        <Pressable style={styles.page} onPress={() => handlePress('History')}>
+          <FontAwesome name='history' style={styles.icon} />
+          <Text style={styles.text}>History</Text>
+        </Pressable>
       </View>
     </TouchableWithoutFeedback>
   )
@@ -31,7 +41,7 @@ const styles = StyleSheet.create({
     width: '70%',
     height: '100%',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -40,6 +50,21 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.58,
     shadowRadius: 16.0,
 
-    elevation: 24
+    elevation: 24,
+    paddingHorizontal: 20
+  },
+  page: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    color: 'black'
+  },
+  icon: {
+    paddingHorizontal: 20,
+    fontSize: 20
+  },
+  text: {
+    fontSize: 20
   }
 })
