720ac8359b6a3318f9a77cb2db79f3c3b0cf1432
Huishi <anhuishi95@gmail.com>
2023-03-06 14:02:13 -0800

    Move database setup to App.js

--- react-app/App.js
+++ react-app/App.js
@@ -6,10 +6,81 @@ import Setting from './src/Setting'
 import History from './src/History'
 import ExerciseOverview from './src/ExerciseOverview'
 import ExerciseSteps from './src/ExerciseSteps'
+import { useEffect, useState } from 'react'
 
 const Stack = createNativeStackNavigator()
+import * as SQLite from 'expo-sqlite'
+
+function openDatabase() {
+  if (Platform.OS === 'web') {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        }
+      },
+    }
+  }
+
+  const db = SQLite.openDatabase('db.db')
+  return db
+}
+
+const db = openDatabase()
 
 export default function App() {
+  const [debugMessage, setDebugMessage] = useState('debug2')
+
+  useEffect(() => {
+    db.transaction(
+      (tx) => {
+        // setDebugMessage('in tx2')
+
+        tx.executeSql(
+          `create table if not exists sample_table (id integer primary key not null, name text);`,
+          [],
+          () => {
+            setDebugMessage('create table success')
+          },
+          () => {
+            setDebugMessage('create table faile')
+            return false
+          }
+        )
+
+        tx.executeSql(
+          `insert into sample_table (name) values (?);`,
+          ['foobar'],
+          () => {
+            setDebugMessage('insert success')
+          },
+          (tx, error) => {
+            setDebugMessage('insert faile' + error)
+            return false
+          }
+        )
+
+        tx.executeSql(
+          `select * from sample_table;`,
+          [],
+          (_, { rows }) => {
+            setDebugMessage('select result:' + JSON.stringify(rows._array))
+          },
+          (tx, error) => {
+            setDebugMessage('select faile' + error)
+            return false
+          }
+        )
+      },
+      (error) => {
+        // setDebugMessage('tx failed')
+      },
+      () => {
+        // setDebugMessage('tx success')
+      }
+    )
+  }, [])
+
   return (
     <NavigationContainer>
       <Stack.Navigator
--- react-app/src/History.js
+++ react-app/src/History.js
@@ -3,84 +3,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import { Calendar } from 'react-native-calendars'
 import NavBar from './NavBar.js'
 import SideMenu from './SideMenu.js'
-import * as SQLite from "expo-sqlite";
 
-
-function openDatabase() {
-  if (Platform.OS === "web") {
-    return {
-      transaction: () => {
-        return {
-          executeSql: () => {},
-        };
-      },
-    };
-  }
-
-  const db = SQLite.openDatabase("db.db");
-  return db;
-}
-
-const db = openDatabase();
-
-export default function History() {
-
+export default function History({ debugMessage }) {
   const [showMenu, setShowMenu] = useState(false)
-  const [debugMessage, setDebugMessage] = useState("debug2");
 
   useEffect(() => {
-    db.transaction(
-      (tx) => {
-        // setDebugMessage('in tx2')
-        
-        tx.executeSql(
-          `create table if not exists sample_table (id integer primary key not null, name text);`,
-          [],
-          () => {
-            setDebugMessage('create table success')
-          },
-          () => {
-            setDebugMessage('create table faile')
-            return false
-          }
-        )
-
-        tx.executeSql(
-          `insert into sample_table (name) values (?);`,
-          ["foobar"],
-          () => {
-            setDebugMessage('insert success')
-          },
-          (tx, error) => {
-            setDebugMessage('insert faile' + error)
-            return false
-          }
-        )
-
-        tx.executeSql(
-          `select * from sample_table;`,
-          [],
-          (_, { rows }) => {
-            setDebugMessage('select result:' + JSON.stringify(rows._array))
-          },
-          (tx, error) => {
-            setDebugMessage('select faile' + error)
-            return false
-          }
-        )
-
-      }, 
-      (error) => {
-        // setDebugMessage('tx failed')
-      },
-      () => {
-        // setDebugMessage('tx success')
-      }
-    );
-
-    
-  }, []);
-
+    alert(debugMessage)
+  }, [debugMessage])
 
   // marked the days
   const marked = {
@@ -105,10 +34,12 @@ export default function History() {
         <View style={styles.recordInfo}>
           <Text style={styles.recordDate}>2023年2月23日</Text>
           <Text style={styles.recordText}>Standard </Text>
-          <Text style={styles.recordText} selectable={true} >  {debugMessage} </Text>
+          <Text style={styles.recordText} selectable={true}>
+            {' '}
+            {debugMessage}{' '}
+          </Text>
         </View>
       </View>
-
     </View>
   )
 }
