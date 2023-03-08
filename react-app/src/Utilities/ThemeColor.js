bc5fab0599fdd98891814a0c762433a2cd510b16
Huishi <anhuishi95@gmail.com>
2023-03-08 14:54:02 -0800

    Add ThemeColor

---
+++ react-app/src/Utilities/ThemeColor.js
@@ -0,0 +1,14 @@
+export const backgroundColor = {
+  dark: '#1C1B20',
+  light: '#FEFEFE',
+}
+
+export const componentColor = {
+  dark: '#465155',
+  light: '#D0D7DD',
+}
+
+export const textColor = {
+  dark: '#FEFEFE',
+  light: '#1C1B20',
+}
