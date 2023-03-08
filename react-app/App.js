36b7ed3c7759056033b5da364ad53bf8108a9b63
j3rry <j3rryzh@gmail.com>
2023-03-08 11:24:52 -0800

    Modify code format

--- react-app/App.js
+++ react-app/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './src/Home'
@@ -7,9 +7,10 @@ import History from './src/History'
 import ExerciseOverview from './src/ExerciseOverview'
 import ExerciseSteps from './src/ExerciseSteps'
 import { Platform } from 'react-native'
-import { useEffect, useState } from 'react'
-const Stack = createNativeStackNavigator()
+import Finish from './src/Finish'
 import * as SQLite from 'expo-sqlite'
+
+const Stack = createNativeStackNavigator()
 
 function openDatabase() {
   if (Platform.OS === 'web') {
