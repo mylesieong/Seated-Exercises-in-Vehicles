9a636e5e4d8abecbcd9b2e934b2809ab29b3802e
Huishi <anhuishi95@gmail.com>
2023-03-09 12:58:16 -0800

    Change to color index

--- react-app/src/Utilities/ThemeColor.js
+++ react-app/src/Utilities/ThemeColor.js
@@ -1,14 +1,22 @@
-export const backgroundColor = {
+const backgroundColor = {
   dark: '#1C1B20',
   light: '#FEFEFE',
 }
 
-export const componentColor = {
+const componentColor = {
   dark: '#465155',
   light: '#D0D7DD',
 }
 
-export const textColor = {
+const textColor = {
   dark: '#FEFEFE',
   light: '#1C1B20',
 }
+
+const ThemeColor = {
+  backgroundColor,
+  componentColor,
+  textColor,
+}
+
+export default ThemeColor
