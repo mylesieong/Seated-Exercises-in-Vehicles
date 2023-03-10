d95f65a33b843bee7b291df5f7df776ed90a42e2
Huishi <anhuishi95@gmail.com>
2023-03-09 17:37:17 -0800

    Add a button to remove all records

--- react-app/src/Setting.js
+++ react-app/src/Setting.js
@@ -1,17 +1,50 @@
-import { StyleSheet, View, Text, Appearance } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, View, Text, Appearance, Pressable } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import NavBar from './NavBar.js'
 import SideMenu from './SideMenu.js'
 import ThemeColor from './Utilities/ThemeColor.js'
 
-export default function Setting() {
+export default function Setting({ db }) {
   const [showMenu, setShowMenu] = useState(false)
+  const [debugMessage, setDebugMessage] = useState('')
+
+  const clearRecords = () => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          `DELETE FROM Record;`,
+          [],
+          (_, rows) => {
+            setDebugMessage('drop table success')
+            alert('All records have been removed')
+          },
+          (_, error) => {
+            setDebugMessage('select failed' + error)
+            if (__DEV__) alert(error)
+            return false
+          }
+        )
+      },
+      (error) => {
+        setDebugMessage('tx failed' + error)
+        if (__DEV__) alert(error)
+      }
+    )
+  }
 
   return (
     <View style={styles.container}>
       <NavBar setShowMenu={setShowMenu} />
       {showMenu && <SideMenu setShowMenu={setShowMenu} />}
-      <Text>Coming soon ... </Text>
+      <View style={styles.line}>
+        <Text style={styles.text}>General</Text>
+      </View>
+      <View style={styles.line}>
+        <Text style={styles.text}>Remove all records</Text>
+        <Pressable style={styles.button} onPress={clearRecords}>
+          <Text style={styles.text}>Remove</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
@@ -24,5 +57,24 @@ const styles = StyleSheet.create({
     backgroundColor: ThemeColor.backgroundColor[colorScheme],
     alignItems: 'flex-start',
     justifyContent: 'flex-start',
+  },
+  text: {
+    color: ThemeColor.textColor[colorScheme],
+    fontSize: 20,
+  },
+  line: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    paddingBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#FFCA28',
+    borderRadius: 5,
+    color: ThemeColor.textColor[colorScheme],
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
 })
