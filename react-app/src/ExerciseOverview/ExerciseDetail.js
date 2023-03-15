2ea5dd180cd3e40481bd7e0b7ef13dc3c5dde609
Huishi <anhuishi95@gmail.com>
2023-03-15 10:28:02 -0700

    Add borderRadius and shawdow and FlatList for instruction

--- react-app/src/ExerciseOverview/ExerciseDetail.js
+++ react-app/src/ExerciseOverview/ExerciseDetail.js
@@ -1,45 +1,66 @@
 import React from 'react'
-import { StyleSheet, Text, ScrollView, View, Pressable } from 'react-native'
-import { SvgXml } from 'react-native-svg'
+import { StyleSheet, Text, SafeAreaView, View, Pressable, FlatList } from 'react-native'
 import CancelIcon from '../../assets/icons/cancel.svg'
-
+import ThemeColor from '../Utilities/ThemeColor'
 import ExerciseImage from '../Utilities/ExerciseImage'
 
 export default function ExerciseDetail({ item, setShowDetail }) {
   const { title, description, instruction, image } = item
   return (
-    <ScrollView style={styles.container}>
-      <Pressable style={styles.buttonContainer} onPress={() => setShowDetail(false)}>
-        <View style={styles.button}>
-          <CancelIcon />
-          <Text> Close </Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Pressable style={styles.buttonContainer} onPress={() => setShowDetail(false)}>
+          <View style={styles.button}>
+            <CancelIcon />
+            <Text> Close </Text>
+          </View>
+        </Pressable>
+        <View style={styles.image}>
+          <ExerciseImage
+            image={image}
+            containerHeight={260}
+            containerWidth={380}
+            containerRadius={10}
+          />
         </View>
-      </Pressable>
-      <View style={styles.image}>
-        <ExerciseImage
-          image={image}
-          containerHeight={260}
-          containerWidth={380}
-          containerRadius={10}
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+        <FlatList
+          data={instruction}
+          renderItem={({ item }) => <Text style={styles.instruction}>{item}</Text>}
         />
       </View>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.description}>{description}</Text>
-      <Text style={styles.instruction}>{instruction}</Text>
-    </ScrollView>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     position: 'absolute',
-    backgroundColor: '#ffffff',
-    top: 100,
+    backgroundColor: ThemeColor.backgroundColor,
+    top: 150,
     left: 0,
-    width: '100%',
     height: '100%',
+    width: '100%',
     zIndex: 10,
     padding: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: -3,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 10,
+
+    elevation: 24,
+  },
+  innerContainer: {
+    flex: 1,
+    backgroundColor: ThemeColor.backgroundColor,
+    padding: 20,
+    borderRadius: 10,
   },
   buttonContainer: {
     display: 'flex',
@@ -49,11 +70,13 @@ const styles = StyleSheet.create({
   },
   button: {
     display: 'flex',
-    backgroundColor: '#F2F2F2',
+    backgroundColor: ThemeColor.componentColor,
     flexDirection: 'row',
+    alignItems: 'center',
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 10,
+    color: ThemeColor.textColor,
   },
   image: {
     display: 'flex',
@@ -65,10 +88,18 @@ const styles = StyleSheet.create({
     fontWeight: 700,
     fontSize: 25,
     marginBottom: 10,
+    color: ThemeColor.textColor,
   },
   description: {
     fontWeight: 500,
     fontSize: 16,
     marginBottom: 10,
+    color: ThemeColor.textColor,
+  },
+  instruction: {
+    fontWeight: 400,
+    fontSize: 16,
+    marginBottom: 10,
+    color: ThemeColor.textColor,
   },
 })
