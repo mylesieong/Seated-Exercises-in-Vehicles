c44da0851b651a94abe8d077b77719be7806ec6a
aka0039 <yumeno.13579@gmail.com>
2023-03-17 02:53:26 -0700

    add history of ui

--- react-app/src/History.js
+++ react-app/src/History.js
@@ -6,6 +6,8 @@ import SideMenu from './SideMenu.js'
 import ThemeColor from './Utilities/ThemeColor.js'
 import PageTemplate from './Utilities/PageTemplate.js'
 
+import { STRETCHING_EXERCISE_DATA } from '../data/StretchingExerciseData'
+
 const toYYYYMMDD = (timestamp) => {
   const date = new Date(timestamp)
   const year = date.getFullYear()
@@ -18,22 +20,21 @@ const toYYYYMMDD = (timestamp) => {
   return [year, month, day].join('-')
 }
 
-// format for view ('YYYY-MM-DD HH:MM')
+// format for view ('HH:MM')
 const formatDate = (timestamp) => {
   const date = new Date(timestamp)
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
   const hour = date.getHours()
   const minute = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`
-  return `${year}-${month}-${day} ${hour}:${minute}`
+  return `${hour}:${minute}`
 }
 
 export default function History({ db, reset }) {
   const [showMenu, setShowMenu] = useState(false)
   const [records, setRecords] = useState({})
   const today = toYYYYMMDD(new Date().getTime())
-  const [selected, setSelected] = useState({ [today]: { selected: true } })
+  const [selected, setSelected] = useState({
+    [today]: { selected: true, selectedColor: '#FFCD28' },
+  })
   const [recordsOfSelected, setRecordsOfSelected] = useState([])
 
   useEffect(() => {
@@ -42,7 +43,7 @@ export default function History({ db, reset }) {
         let result = rows._array.reduce(
           (previous, current) => ({
             ...previous,
-            [toYYYYMMDD(current.timestamp)]: { marked: true },
+            [toYYYYMMDD(current.timestamp)]: { marked: true, selectedColor: '#FFCD28' },
           }),
           {}
         )
@@ -65,86 +66,103 @@ export default function History({ db, reset }) {
     <PageTemplate>
       <NavBar setShowMenu={setShowMenu} />
       {showMenu && <SideMenu setShowMenu={setShowMenu} />}
-      <View style={styles.calendar}>
-        <Calendar
-          markedDates={{ ...records, ...selected }}
-          onDayPress={(day) => {
-            const localTimestamp = day.timestamp + new Date().getTimezoneOffset() * 60 * 1000
-            setSelected({ [day.dateString]: { selected: true } })
-            db.transaction((tx) => {
-              tx.executeSql(
-                'select timestamp, exercise_name from Record where timestamp between ? and ? order by timestamp',
-                [localTimestamp, localTimestamp + 24 * 60 * 60 * 1000 - 1],
-                (_, { rows }) => {
-                  setRecordsOfSelected(rows._array)
-                }
-              )
-            })
-          }}
-          onMonthChange={() => {
-            setRecordsOfSelected([])
-          }}
-          hideExtraDays={true}
-          theme={{
-            calendarBackground: ThemeColor.backgroundColor,
-            dayTextColor: ThemeColor.textColor,
-            arrowColor: ThemeColor.textColor,
-            monthTextColor: ThemeColor.textColor,
-          }}
-        />
+      <View style={styles.container}>
+        <View style={styles.calendarContainer}>
+          <View style={styles.calendar}>
+            <Calendar
+              markedDates={{ ...records, ...selected }}
+              onDayPress={(day) => {
+                const localTimestamp = day.timestamp + new Date().getTimezoneOffset() * 60 * 1000
+                setSelected({
+                  [day.dateString]: {
+                    selected: true,
+                    dotColor: 'blue',
+                    selectedColor: '#FFCD28',
+                  },
+                })
+                db.transaction((tx) => {
+                  tx.executeSql(
+                    'select timestamp, exercise_name from Record where timestamp between ? and ? order by timestamp',
+                    [localTimestamp, localTimestamp + 24 * 60 * 60 * 1000 - 1],
+                    (_, { rows }) => {
+                      setRecordsOfSelected(rows._array)
+                    }
+                  )
+                })
+              }}
+              onMonthChange={() => {
+                setRecordsOfSelected([])
+              }}
+              hideExtraDays={true}
+              theme={{
+                calendarBackground: ThemeColor.backgroundColor,
+                dayTextColor: ThemeColor.textColor,
+                arrowColor: ThemeColor.textColor,
+                monthTextColor: ThemeColor.textColor,
+              }}
+            />
+          </View>
+          <Text style={styles.textTitle}>Exercise Record</Text>
+        </View>
         {/* Text of the record */}
-        <Text style={[styles.title, !recordsOfSelected[0] && styles.hide]}>Records</Text>
-        <FlatList
-          data={recordsOfSelected}
-          renderItem={({ item }) => (
-            <View style={styles.recordInfo}>
-              <Text style={styles.recordText}>{formatDate(item.timestamp)}</Text>
-              <Text style={styles.recordText}>{item.exercise_name}</Text>
-            </View>
-          )}
-        />
-        {__DEV__ && (
-          <Button
-            title='PRESS TO ADD 100 RANDOM DATA'
-            color='#841584'
-            onPress={() => {
-              Array(100)
-                .fill(1)
-                .forEach(() => {
-                  db.transaction((tx) => {
-                    const randomTimeInMarch = Math.floor(
-                      Math.random() * (1680307200000 - 1677628800000) + 1677628800000
+        <View>
+          <FlatList
+            data={recordsOfSelected}
+            renderItem={({ item }) => (
+              <View style={styles.recordContainer}>
+                <View style={styles.recordInfo}>
+                  <Text style={styles.exerciseName}>{item.exercise_name}</Text>
+                  <Text style={styles.timeStamp}>{formatDate(item.timestamp)}</Text>
+                </View>
+                <Text style={styles.exerciseDetail}>
+                  {STRETCHING_EXERCISE_DATA.length} Moves, 10 minutes
+                </Text>
+              </View>
+            )}
+          />
+          {__DEV__ && (
+            <Button
+              title='PRESS TO ADD 100 RANDOM DATA'
+              color='#841584'
+              onPress={() => {
+                Array(100)
+                  .fill(1)
+                  .forEach(() => {
+                    db.transaction((tx) => {
+                      const randomTimeInMarch = Math.floor(
+                        Math.random() * (1680307200000 - 1677628800000) + 1677628800000
+                      )
+                      tx.executeSql('insert into Record (exercise_name, timestamp) values (?, ?)', [
+                        'just for testing',
+                        randomTimeInMarch,
+                      ])
+                    })
+                  })
+                db.transaction((tx) => {
+                  tx.executeSql('select * from Record', [], (_, { rows }) => {
+                    let result = rows._array.reduce(
+                      (previous, current) => ({
+                        ...previous,
+                        [toYYYYMMDD(current.timestamp)]: { marked: true, selectedColor: '#FFCD28' },
+                      }),
+                      {}
                     )
-                    tx.executeSql('insert into Record (exercise_name, timestamp) values (?, ?)', [
-                      'just for testing',
-                      randomTimeInMarch,
-                    ])
+                    setRecords(result)
                   })
                 })
-              db.transaction((tx) => {
-                tx.executeSql('select * from Record', [], (_, { rows }) => {
-                  let result = rows._array.reduce(
-                    (previous, current) => ({
-                      ...previous,
-                      [toYYYYMMDD(current.timestamp)]: { marked: true },
-                    }),
-                    {}
-                  )
-                  setRecords(result)
-                })
-              })
-            }}
-          />
-        )}
-        {__DEV__ && (
-          <Button
-            title='console log'
-            color='#005408'
-            onPress={() => {
-              console.log(recordsOfSelected)
-            }}
-          />
-        )}
+              }}
+            />
+          )}
+          {__DEV__ && (
+            <Button
+              title='console log'
+              color='#005408'
+              onPress={() => {
+                console.log(recordsOfSelected)
+              }}
+            />
+          )}
+        </View>
       </View>
     </PageTemplate>
   )
@@ -152,30 +170,63 @@ export default function History({ db, reset }) {
 
 // Css
 const styles = StyleSheet.create({
-  calendar: {
-    marginTop: 80,
+  container: {
     ...StyleSheet.absoluteFillObject,
-    paddingHorizontal: 15,
   },
-  title: {
-    margin: 15,
+  calendarContainer: {
+    marginTop: 50,
+    paddingVertical: 10,
+    backgroundColor: '#FFFBED',
+    height: 370,
+  },
+  calendar: {
+    borderWidth: 7,
+    borderColor: '#FFFFFF',
+    borderTopColor: '#FFB300',
+    borderRadius: 10,
+    marginHorizontal: 20,
+    shadowOffset: { width: 0.5, height: 0.5 },
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    // for android
+    elevation: 1,
+  },
+  textTitle: {
+    padding: 10,
     fontSize: 15,
     fontWeight: 'bold',
-    textAlign: 'center',
+    textAlign: 'left',
+    color: '#979797',
+  },
+  recordContainer: {
+    borderWidth: 3,
+    borderColor: '#FFFFFF',
+    borderBottomColor: '#F5F5F5',
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  recordInfo: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'left',
     color: ThemeColor.textColor,
+  },
+  timeStamp: {
+    fontSize: 15,
+    textAlign: 'right',
+  },
+  exerciseDetail: {
+    paddingLeft: 10,
+    paddingBottom: 10,
+    fontSize: 15,
   },
   hide: {
     display: 'none',
-  },
-  recordInfo: {
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
-    flexDirection: 'row',
-  },
-  recordText: {
-    margin: 9,
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: ThemeColor.textColor,
   },
 })
