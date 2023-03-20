98be483c01ccad92add6c52828230bdf4aee8c57
j3rry <j3rryzh@gmail.com>
2023-03-20 01:58:52 -0700

    Fixes #306 #305 and adjust layout, use font

--- react-app/src/ExerciseOverview/index.js
+++ react-app/src/ExerciseOverview/index.js
@@ -1,12 +1,13 @@
-import { FlatList, Pressable, Platform } from 'react-native'
+import { FlatList, Pressable, Platform, Text, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
-import Header from './Header'
+import Header from '../Utilities/Header'
 import Summary from './Summary'
 import ExerciseCard from './ExerciseCard'
 import StartExerciseButton from './StartExerciseButton'
 import { STRETCHING_EXERCISE_DATA } from '../../data/StretchingExerciseData'
 import PageTemplate from '../Utilities/PageTemplate'
 import ExerciseDetail from './ExerciseDetail'
+import ThemeColor from '../Utilities/ThemeColor'
 
 export default function ExerciseOverview() {
   const [showDetail, setShowDetail] = React.useState(false)
@@ -20,7 +21,14 @@ export default function ExerciseOverview() {
   return (
     <PageTemplate topBarColor={'#FFB300'}>
       <Pressable style={{ zIndex: 2 }} onPress={() => setShowDetail(false)}>
-        <Header />
+        <Header
+          navigation={'Home'}
+          backgroundColor={'#FFB300'}
+          buttonColor={ThemeColor.titleTextColor}
+          height={190}
+        >
+          <Text style={styles.title}>Stretching on the Seat</Text>
+        </Header>
         <Summary moves={STRETCHING_EXERCISE_DATA.length} time={`10`} format={`Sitting`}></Summary>
       </Pressable>
       <FlatList
@@ -41,3 +49,14 @@ export default function ExerciseOverview() {
     </PageTemplate>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 15,
+    lineHeight: 20,
+    color: ThemeColor.titleTextColor,
+    marginTop: 13,
+    fontFamily: 'NotoSansBold',
+    transform: [{ scaleX: 0.875 }],
+  },
+})
---
+++ react-app/src/Utilities/Header.js
@@ -0,0 +1,38 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+import GoBackButton from './GoBackButton'
+import { useNavigation } from '@react-navigation/native'
+import ThemeColor from './ThemeColor'
+
+export default function Header({
+  navigation: navigationPage,
+  children,
+  backgroundColor,
+  buttonColor,
+  height,
+}) {
+  const navigation = useNavigation()
+  return (
+    <View
+      style={[
+        styles.header,
+        backgroundColor
+          ? { backgroundColor: backgroundColor }
+          : { backgroundColor: ThemeColor.backgroundColor },
+        height && { height: height },
+      ]}
+    >
+      <GoBackButton
+        onPress={() => navigation.navigate(navigationPage)}
+        color={buttonColor ? buttonColor : ThemeColor.textColor}
+      />
+      {children}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+  },
+})
