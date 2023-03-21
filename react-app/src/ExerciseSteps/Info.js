bae1f1e63047432547f98a0a32c96add681a1fe9
Huishi <anhuishi95@gmail.com>
2023-03-20 21:11:58 -0700

    Fix FlatList visibility issue of last item

--- react-app/src/ExerciseSteps/Info.js
+++ react-app/src/ExerciseSteps/Info.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, FlatList, Pressable } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import React from 'react'
 import ThemeColor from '../Utilities/ThemeColor'
 import QuestionMark from '../../assets/icons/questionmark.svg'
 
